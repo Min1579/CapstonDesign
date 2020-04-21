113b2ce4e923e7cd4fd2e6aed54efdcbed55bf1d
min <hsm012362@gmail.com>
2020-04-21 16:11:20 +0900

    project init

---
+++ heythere_backend/src/test/java/org/devs/heythere_backend/HeythereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devs.heythere_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeythereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
